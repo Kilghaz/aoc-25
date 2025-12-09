79c6d8d1feeb17ae4a951ff9b0175a52a98bb683
Philipp Jean-Jacques <philipp.jean-jacques@iits-consulting.de>
2025-12-09 16:46:28 +0100

    feat: day 4, part 2

--- day_4/part-2.go
+++ day_4/part-2.go
@@ -1,4 +1,30 @@
 package day_4
 
+import (
+	"aoc25/parser"
+
+	"github.com/samber/lo"
+)
+
 func Part2(input string) {
+	nodes := parsePaperGrid(input)
+
+	removedPaperRolls := 0
+
+	for {
+		accessiblePaperRolls := lo.Filter(nodes, isPaperRollAccessible)
+
+		if len(accessiblePaperRolls) == 0 {
+			break
+		}
+
+		removedPaperRolls += len(accessiblePaperRolls)
+		lo.ForEach(accessiblePaperRolls, func(item *parser.Node[bool], index int) {
+			item.Value = false // remove paper roll
+		})
+
+		nodes = lo.Filter(nodes, isPaperRoll)
+	}
+
+	println(removedPaperRolls)
 }
