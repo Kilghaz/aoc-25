f97b9d1eab5009070492244c7df1ee6b19074a63
Philipp Jean-Jacques <philipp.jean-jacques@iits-consulting.de>
2025-12-09 16:03:17 +0100

    feat: day 3, part 1

--- day_3/part-1.go
+++ day_3/part-1.go
@@ -2,7 +2,7 @@ package day_3
 
 import "github.com/samber/lo"
 
-func findHighestJoltage(batteries []int) int {
+func findHighestJoltageWithTwoBatteries(batteries []int) int {
 	highestLeft := lo.Max(batteries[0 : len(batteries)-1])
 	_, start, _ := lo.FindIndexOf(batteries[0:len(batteries)-1], func(item int) bool {
 		return item == highestLeft
@@ -17,7 +17,7 @@ func Part1(input string) {
 	banks := parseBatteriesBanks(input)
 
 	joltages := lo.Map(banks, func(batteries []int, index int) int {
-		return findHighestJoltage(batteries)
+		return findHighestJoltageWithTwoBatteries(batteries)
 	})
 
 	println(lo.Sum(joltages))
